ab13c9660d0798781581dbc73cd5cb27d18c288c
jin-park0115 <122075848+jin-park0115@users.noreply.github.com>
2024-03-23 00:39:56 +0900

    11번 꼬마정민 더하기문제

---
+++ 단계별_JS/11_꼬마정민.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync(0, "utf-8").toString().split(" ");
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+const c = Number(input[2]);
+
+console.log(a + b + c);
